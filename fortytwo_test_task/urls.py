c6a7ab0858b04ad54836a522e30a77bf1d023cfd
kyrylo <forkirill@i.ua>
2016-04-13 23:18:35 +0300

    ticket:1 template refactoring and added / to urls

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     '',
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
+    url(r'^$', 't1_contact.views.index', name='index'),
     url(r'^t1/', include('t1_contact.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
