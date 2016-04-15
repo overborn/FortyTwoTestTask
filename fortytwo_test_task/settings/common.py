5209478c01ebce8efbe358a487a9c8ac9db9cc66
kyrylo <forkirill@i.ua>
2016-04-15 22:11:15 +0300

    ticket:1 fixtures moved to app folder

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -127,9 +127,5 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
 
-FIXTURE_DIRS = (
-    os.path.join(BASE_DIR, 'fixtures'),
-)
-
 # Turn off south during test
 SOUTH_TESTS_MIGRATE = False
