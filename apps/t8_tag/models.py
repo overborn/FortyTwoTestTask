104a64d007084fbd2a3fdb6028cd301759ec619e
kyrylo <forkirill@i.ua>
2016-04-23 21:23:28 +0300

    ticket:8 tests for model logging added (tests fail)

---
+++ apps/t8_tag/models.py
@@ -0,0 +1,5 @@
+from django.db.models import models
+
+
+class ModelLogEntry(models.Model):
+    pass
--- apps/t8_tag/tests.py
+++ apps/t8_tag/tests.py
@@ -2,9 +2,11 @@ from django.core.urlresolvers import reverse
 from django.core.management import call_command
 from django.conf import settings
 from django.test import TestCase
+from django.test.utils import override_settings
 from django.template import Context, Template, TemplateSyntaxError
 from StringIO import StringIO
 from t1_contact.models import Person
+from t8_tag.models import ModelLogEntry
 import os
 import time
 import subprocess
@@ -59,32 +61,61 @@ class AdminLinksTests(TestCase):
 class CommandTests(TestCase):
     fixtures = ['initial_data.json']
 
-    def setUp(self):
+    def call(self):
         self.out = StringIO()
         self.err = StringIO()
-
-    def call(self):
         return call_command('printmodels', stdout=self.out, stderr=self.err)
 
-    def test_command(self):
+    def test_command_traces_deletion_of_instances(self):
+        """
+        chacks if command traces deletion of instances
+        """
+        count = Person.objects.count()
         self.call()
-        self.assertIn('model Person has 1 instances', self.out.getvalue())
         self.assertIn(
-            'error: model Request has 0 instances', self.err.getvalue())
+            'model Person has {} instances'.format(count),
+            self.out.getvalue()
+        )
+        self.assertIn(
+            'error: model Person has {} instances'.format(count),
+            self.err.getvalue()
+        )
+        Person.objects.get(pk=1).delete()
+        count2 = Person.objects.count()
+        self.assertEqual(count2, count - 1)
+        self.call()
+        self.assertIn(
+            'model Person has {} instances'.format(count2),
+            self.out.getvalue()
+        )
+        self.assertIn(
+            'error: model Person has {} instances'.format(count2),
+            self.err.getvalue()
+        )
 
-    def test_command_traces_number_of_instances(self):
+    def test_command_traces_addition_of_instances(self):
+        """
+        chacks if command traces addition of instances
+        """
         count = Person.objects.count()
         self.call()
         self.assertIn(
             'model Person has {} instances'.format(count), self.out.getvalue())
+        self.assertIn(
+            'error: model Person has {} instances'.format(count),
+            self.err.getvalue()
+        )
         person = Person()
         person.save()
         count2 = Person.objects.count()
         self.assertEqual(count2, count + 1)
-        self.out = StringIO()
         self.call()
         self.assertIn(
             'model Person has {} instances'.format(count2), self.out.getvalue()
+        )
+        self.assertIn(
+            'error: model Person has {} instances'.format(count2),
+            self.err.getvalue()
         )
 
     def test_script_creates_file_with_stderr_output(self):
@@ -102,3 +133,36 @@ class CommandTests(TestCase):
             'error: model Person has {} instances'.format(count),
             open(name).read()
         )
+
+
+@override_settings(ENABLE_MODEL_LOGGING=True)
+class ModelLoggingTests(TestCase):
+
+    def test_saving_and_deleting_creates_model_log_entry(self):
+        """
+        checks if ModelLogEntry is created after save and delete actions
+        """
+        self.assertEqual(ModelLogEntry.objects.count(), 0)
+        person = Person()
+        person.save()
+
+        self.assertEqual(ModelLogEntry.objects.count(), 1)
+        entry = ModelLogEntry.objects.order_by('created').last()
+        self.assertEqual(entry.action, 'CREATE')
+        self.assertEqual(entry.model, 'Person')
+        person.first_name = 'Name'
+        person.last_name = 'Surname'
+        person.save()
+
+        self.assertEqual(ModelLogEntry.objects.count(), 2)
+        entry = ModelLogEntry.objects.order_by('created').last()
+        self.assertEqual(entry.action, 'UPDATE')
+        self.assertEqual(entry.model, 'Person')
+        self.assertEqual(entry.instance, 'Name Surname')
+        person.delete()
+
+        self.assertEqual(ModelLogEntry.objects.count(), 3)
+        entry = ModelLogEntry.objects.order_by('created').last()
+        self.assertEqual(entry.action, 'DELETE')
+        self.assertEqual(entry.model, 'Person')
+        self.assertEqual(entry.instance, 'Name Surname')
