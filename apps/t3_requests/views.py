f399d407ab935ee1759455b85b4d0b463379c386
kyrylo <forkirill@i.ua>
2016-04-30 01:19:59 +0300

    ticket:13 priority changing implemented (tests pass)

--- apps/t3_requests/views.py
+++ apps/t3_requests/views.py
@@ -12,6 +12,25 @@ def requests(request, template='requests.html'):
     requests = Request.objects.order_by('-' + order)[:10]
     logger.info(requests)
     if request.is_ajax():
-        requests = [{'id': r.id, 'string': str(r)} for r in requests]
+        requests = [{
+            'id': r.id,
+            'created': r.created.strftime('%Y-%m-%d %H:%M:%S'),
+            'method': r.method,
+            'path': r.path,
+            'query': r.query,
+            'priority': r.priority,
+            'user': unicode(r.user),
+        } for r in requests]
         return {'requests': requests}
     return render(request, template, {'requests': requests})
+
+
+@json_view
+def change_priority(request):
+    value = request.GET.get('value')
+    request_id = request.GET.get('id')
+    request = Request.objects.get(pk=request_id)
+    request.priority += int(value)
+    request.save()
+    logger.info(request)
+    return {'status': 'ok'}
